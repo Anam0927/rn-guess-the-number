95f0c3a2b880d9e9ca99b798f94fa8306ed15b60
Anam0927 <anam.devdes27@gmail.com>
2022-08-18 20:32:23 +0530

    chore / basic setup complete

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './colors';
 
 export default function App() {
-  return (
+  const [loaded] = useFonts({
+    'Josefin Sans Bold': require('./assets/fonts/JosefinSans-Bold.ttf'),
+    'Josefin Sans': require('./assets/fonts/JosefinSans-Regular.ttf'),
+  });
+
+  return loaded ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  ) : null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.primary.black,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontFamily: 'Josefin Sans Bold',
+    fontSize: 32,
+    textAlign: 'center',
+    color: colors.primary.main,
   },
 });
---
+++ colors.js
@@ -0,0 +1,21 @@
+export const colors = {
+  primary: {
+    black: '#1a0800',
+    highlight: '#3a0088',
+    main: '#d98341',
+    white: '#fffefc',
+  },
+  shades: {
+    100: '#f7cb94',
+    200: '#e6ac73',
+    300: '#d98341',
+    400: '#a64819',
+  },
+  greys: {
+    100: '#e6e0d5',
+    200: '#ccc2b8',
+    300: '#b3a398',
+    400: '#99867a',
+    500: '#806f66',
+  },
+};
